71d8c7a7efcb1975d5d4db1a994a414942fa607e
Skye.Wu <skye.wu@zyxel.com.tw>
2018-04-19 17:18:03 +0800

    commit
    Signed-off-by: Skye.Wu <skye.wu@zyxel.com.tw>

--- index.js
+++ index.js
@@ -2,15 +2,46 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const fs = require('fs');
+const bodyParser = require('body-parser');
+
+let houses;
 
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
 }));
 
+app.use(bodyParser.json());
+
 app.get('/houseList', function (req, res, next) {
-    const houses = JSON.parse(fs.readFileSync('./houses.json', 'utf-8'));
+    houses = JSON.parse(fs.readFileSync('./houses.json', 'utf-8'));
+
     res.json(houses);
+});
+
+app.post('/house/:id', function (req, res, next) {
+    console.log(req.params);
+
+    res.json({})
+});
+
+app.patch('/house/:houseid', function (req, res, next) {
+    const { houseid } = req.params;
+    const { status } = req.body.data;
+
+    const query_house = houses.find(function (house) {
+        return house.houseid == houseid;
+    });
+    query_house.status = status;
+
+    if (query_house) {
+        fs.writeFile('./houses.json', JSON.stringify(houses, null, 4), 'utf8', function () {
+            res.json({});
+        });
+    } else {
+        res.status(400).json({message: `House Id: ${houseid} is not match.`});
+    }
+
 });
 
 app.listen('4444', () => {
--- js/main.js
+++ js/main.js
@@ -9,10 +9,47 @@
 angular.module('House', [])
     .controller('masterCtrl', ['$q', '$http', '$scope', function ($q, $http, $scope) {
         $http.get('http://localhost:4444/houseList').then(function (resp) {
-            $scope.house_list = resp.data.sort(function (a, b) {
+            $scope.house_list = resp.data.filter(function (house) {
+                return house.status !== 'not_show';
+            }).sort(function (a, b) {
                 if (a.price > b.price) { return 1; }
                 if (a.price < b.price) { return -1; }
                 return 0;
             });
         });
+
+        $scope.not_show = function (house) {
+            $http.patch(`http://localhost:4444/house/${house.houseid}`, {
+                data: {
+                    status: 'not_show'
+                }
+            }).then(function () {
+                house.status = 'not_show';
+            }, function (resp) {
+                alert(resp.message);
+            });
+        };
+
+        $scope.done = function (house) {
+            $http.patch(`http://localhost:4444/house/${house.houseid}`, {
+                data: {
+                    status: 'done'
+                }
+            }).then(function () {
+                house.status = 'done';
+            }, function (resp) {
+                alert(resp.message);
+            });
+        };
+
+        var order = 'asc'
+        $scope.sort = function (property) {
+            $scope.house_list.sort(function (a, b) {
+                if (a[property] > b[property]) { return (order === 'asc') ? -1 : 1; }
+                if (a[property] < b[property]) { return (order === 'asc') ? 1 : -1; }
+                return 0;
+            });
+
+            order = (order === 'asc') ? 'desc' : 'asc';
+        };
     }]);
